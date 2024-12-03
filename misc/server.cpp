c44bd853872d0ab881413bbc453d98160f67d929
Francisco Mónica <francisco.monica@tecnico.ulisboa.pt>
2024-12-03 22:16:54 +0000

    Added server: initial implementations of all except try

--- misc/server.cpp
+++ misc/server.cpp
@@ -53,12 +53,17 @@ static bool has_ongoing_game(int plid) { // TODO: incorporate this in template ?
 }*/
 
 static net::action_status start_new_game(net::stream<net::file_source>& msg, std::string_view host, std::string_view port);
+static net::action_status end_game(net::stream<net::file_source>& msg, std::string_view host, std::string_view port);
+static net::action_status debug_game(net::stream<net::file_source>& msg, std::string_view host, std::string_view port);
 
 int main() {
 	srand(time(0));
 	
 	net::action_map<net::file_source, std::string_view, std::string_view> actions;
 	actions.add_action("SNG", start_new_game);
+	actions.add_action("QUT", end_game);
+	actions.add_action("DBG", debug_game);
+
 	while (!exit_server)  	{
 		net::stream<net::file_source> strm{STDIN_FILENO, false};
 		auto status = actions.execute(strm, "", PORT);
@@ -71,21 +76,44 @@ int main() {
 	return 0;
 }
 
+// dumb func
+static void create_game(int plid, int playtime, std::string key) {
+	PLID = plid;
+	max_playtime = playtime;
+	start_time = std::time(nullptr);
+	secret_key = key;
+	in_game = true;
+}
+
 static net::action_status start_new_game(net::stream<net::file_source>& msg, std::string_view host, std::string_view port) {
+	net::out_stream out_strm;
+	out_strm.write("RSG");
 	auto [status, fields] = msg.read({{4, 6}, {1, 3}});
-	if (status != net::action_status::OK || (status = msg.no_more_fields()) != net::action_status::OK)
+	if (status != net::action_status::OK || (status = msg.no_more_fields()) != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
 		return status;
+	}
 	status = net::is_valid_plid(fields[0]);
-	if (status != net::action_status::OK)
+	if (status != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
 		return status;
+	}
 	
 	status = net::is_valid_max_playtime(fields[1]);
-	if (status != net::action_status::OK)
+	if (status != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
 		return status;
+	}
 	int plid = std::stoi(fields[0]);
 	int playtime = std::stoi(fields[1]);
-	if (in_game)
+	if (in_game) {
+		out_strm.write("NOK").prime();
+		std::cout << out_strm.view();
 		return net::action_status::ONGOING_GAME;
+	}
 
 	std::vector<char> color_vector(valid_colors.begin(), valid_colors.end());
 	std::string key;
@@ -94,12 +122,86 @@ static net::action_status start_new_game(net::stream<net::file_source>& msg, std
 			key += ' ';
 		key += color_vector[rand() % valid_colors.size()];
 	}
-	std::cout << plid << "\t" << playtime << "\t" << key <<"\n";
+	create_game(plid, playtime, key);
+	out_strm.write("OK").prime();
+	std::cout << out_strm.view();
 	
-	PLID = plid;
-	max_playtime = playtime;
-	start_time = std::time(nullptr);
-	secret_key = key;
-	in_game = true;
+	return net::action_status::OK;
+}
+
+static net::action_status end_game(net::stream<net::file_source>& msg, std::string_view host, std::string_view port) {
+	net::out_stream out_strm;
+	out_strm.write("RQT");
+	auto [status, fields] = msg.read({{4, 6}});
+	if (status != net::action_status::OK || (status = msg.no_more_fields()) != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
+		return status;
+	}
+	status = net::is_valid_plid(fields[0]);
+
+	if (status != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
+		return status;
+	}
+	
+	int plid = std::stoi(fields[0]);
+	if (!in_game || plid != PLID) {
+		out_strm.write("NOK").prime();
+		std::cout << out_strm.view();
+		return net::action_status::NOT_IN_GAME;
+	}
+	in_game = false;
+	out_strm.write("OK").write(secret_key).prime();
+	std::cout << out_strm.view();
+	return net::action_status::OK;
+}
+
+static net::action_status debug_game(net::stream<net::file_source>& msg, std::string_view host, std::string_view port) {
+	net::out_stream out_strm;
+	out_strm.write("RDB");
+	auto [status, fields] = msg.read({{4, 6}, {1, 3}, {1, 1}, {1, 1}, {1, 1}, {1, 1}});
+	if (status != net::action_status::OK || (status = msg.no_more_fields()) != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
+		return status;
+	}
+	
+	status = net::is_valid_plid(fields[0]);
+	if (status != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
+		return status;
+	}
+	
+	status = net::is_valid_max_playtime(fields[1]);
+	if (status != net::action_status::OK) {
+		out_strm.write("ERR").prime();
+		std::cout << out_strm.view();
+		return status;
+	}
+
+	int plid = std::stoi(fields[0]);
+	int playtime = std::stoi(fields[1]);
+	
+	if(in_game) {
+		out_strm.write("NOK").prime();
+		std::cout << out_strm.view();
+		return net::action_status::ONGOING_GAME;
+	}
+	net::out_stream key;
+	for (size_t i = 2; i < 2 + GUESS_SIZE; i++) {
+		if ((status = net::is_valid_color(fields[i])) != net::action_status::OK) {
+			out_strm.write("ERR").prime();
+			std::cout << out_strm.view();
+			return status;
+		}
+		key.write(fields[i]);
+	}
+	out_strm.write("OK").prime();
+	std::cout << out_strm.view();
+	create_game(plid, playtime, key.view().data());
+
 	return net::action_status::OK;
 }
